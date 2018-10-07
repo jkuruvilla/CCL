508478cb9fcebbc5fec406bcc737406962165810
Joseph Kuruvilla <joseph.j.kuruvilla@gmail.com>
2018-10-07 19:28:40 +0200

    update

--- .travis/install.sh
+++ .travis/install.sh
@@ -34,5 +34,5 @@ case "${TOXENV}" in
 esac;
 
 source activate test-environment
-conda install --yes numpy nose coveralls flake8 swig gsl
+conda install --yes numpy nose coveralls flake8 swig gsl fftw
 
